89d69bd9aea0399bec57ebff30620ac7ac4446e2
marvin0102 <marvin890102@gmail.com>
2023-11-24 19:47:46 +0800

    Modify q_size function
    follow the guide of lab0 to modify the q_size function.

--- queue.c
+++ queue.c
@@ -14,9 +14,7 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    struct list_head *new = list_head();
-
-    return new;
+    return NULL;
 }
 
 /* Free all storage used by queue */
@@ -49,7 +47,14 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+    int len = 0;
+    struct list_head *li;
+
+    list_for_each (li, head)
+        len++;
+    return len;
 }
 
 /* Delete the middle node in queue */
