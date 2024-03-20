193c28dc7565cff0c11cbb176bd7933b25cd85f6
marvin0102 <marvin890102@gmail.com>
2024-03-20 17:54:17 +0800

    Fix point implementation in Monte Carlo algorithm
    Implement fixed point calculation in MCTS and the
    MCB score with Q formate, which set fraction with
     8 bit and use int32_t to define the signed bit.

--- agents/mcts.c
+++ agents/mcts.c
@@ -1,18 +1,23 @@
 #include <assert.h>
 #include <float.h>
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 #include "game.h"
 #include "mcts.h"
 #include "util.h"
 
+#define EXPLORATION_FACTOR fixed_sqrt(2 << Q)
+#define K (1 << (Q - 1))
+
 struct node {
     int move;
     char player;
     int n_visits;
-    double score;
+    Q15_16 score;
     struct node *parent;
     struct node *children[N_GRIDS];
 };
@@ -37,22 +42,65 @@ static void free_node(struct node *node)
     free(node);
 }
 
-static inline double uct_score(int n_total, int n_visits, double score)
+Q15_16 fixed_sqrt(Q15_16 x)
+{
+    if (x <= 1 << Q) /* Assume x is always positive */
+        return x;
+
+    Q15_16 z = 0;
+    for (Q15_16 m = 1UL << ((31 - __builtin_clz(x)) & ~1UL); m; m >>= 2) {
+        int b = z + m;
+        z >>= 1;
+        if (x >= b)
+            x -= b, z += m;
+    }
+    z = z << Q / 2;
+    return z;
+}
+
+
+Q15_16 fixed_log(int input)
+{
+    if (!input || input == (1U << Q))
+        return 0;
+
+    int64_t y = input << Q;  // int to Q15_16
+    y = ((y - (1U << Q)) << (Q)) / (y + (1U << Q));
+    int64_t ans = 0U;
+    for (unsigned i = 1; i < 20; i += 2) {
+        int64_t z = (1U << Q);
+        for (int j = 0; j < i; j++) {
+            z *= y;
+            z >>= Q;
+        }
+        z <<= Q;
+        z /= (i << Q);
+
+        ans += z;
+    }
+    ans <<= 1;
+    return (Q15_16) ans;
+}
+
+static inline Q15_16 uct_score(int n_total, int n_visits, Q15_16 score)
 {
     if (n_visits == 0)
-        return DBL_MAX;
-    return score / n_visits +
-           EXPLORATION_FACTOR * sqrt(log(n_total) / n_visits);
+        return INT32_MAX;
+    Q15_16 result = score << Q / (Q15_16) (n_visits << Q);
+    int64_t tmp =
+        EXPLORATION_FACTOR * fixed_sqrt(fixed_log(n_total) / n_visits);
+    Q15_16 resultN = tmp >> Q;
+    return result + resultN;
 }
 
 static struct node *select_move(struct node *node)
 {
     struct node *best_node = NULL;
-    double best_score = -1;
+    Q15_16 best_score = INT2Q(-1);
     for (int i = 0; i < N_GRIDS; i++) {
         if (!node->children[i])
             continue;
-        double score = uct_score(node->n_visits, node->children[i]->n_visits,
+        Q15_16 score = uct_score(node->n_visits, node->children[i]->n_visits,
                                  node->children[i]->score);
         if (score > best_score) {
             best_score = score;
@@ -62,7 +110,7 @@ static struct node *select_move(struct node *node)
     return best_node;
 }
 
-static double simulate(char *table, char player)
+static Q15_16 simulate(char *table, char player)
 {
     char current_player = player;
     char temp_table[N_GRIDS];
@@ -87,7 +135,7 @@ static double simulate(char *table, char player)
     return 0.5;
 }
 
-static void backpropagate(struct node *node, double score)
+static void backpropagate(struct node *node, Q15_16 score)
 {
     while (node) {
         node->n_visits++;
@@ -119,13 +167,13 @@ int mcts(char *table, char player)
         memcpy(temp_table, table, N_GRIDS);
         while (1) {
             if ((win = check_win(temp_table)) != ' ') {
-                double score =
+                Q15_16 score =
                     calculate_win_value(win, node->player ^ 'O' ^ 'X');
                 backpropagate(node, score);
                 break;
             }
             if (node->n_visits == 0) {
-                double score = simulate(temp_table, node->player);
+                Q15_16 score = simulate(temp_table, node->player);
                 backpropagate(node, score);
                 break;
             }
@@ -144,7 +192,9 @@ int mcts(char *table, char player)
             best_node = root->children[i];
         }
     }
-    int best_move = best_node->move;
+    int best_move = -1;
+    if (best_node)
+        best_move = best_node->move;
     free_node(root);
     return best_move;
 }
--- agents/mcts.h
+++ agents/mcts.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <stdint.h>
 #define ITERATIONS 100000
-#define EXPLORATION_FACTOR sqrt(2)
+
+
+#define Q 8
+
+#define INT2Q(x) x < 0 ? (uint32_t) x << Q | (1U << 31) : x << Q
+typedef int32_t Q15_16;
 
 int mcts(char *table, char player);
